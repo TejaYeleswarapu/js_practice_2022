c772b84934f5fc232e4546f8164393e30c8f2a83
TejaYeleswarapu <teja.yeleswarapu@gmail.com>
2022-09-11 13:54:16 +0530

    fizzbuzz program

---
+++ programStructure/statementsAndExpressions/ifElseUsingLogicalOperators.js
@@ -0,0 +1,13 @@
+let x = 15;
+if (x % 3 == 0 && x % 5 == 0) {
+console.log ("fizzBuzz");
+}
+else if (x % 3 == 0) {
+console.log ("fizz");
+}
+else if (x % 5 == 0) {
+console.log ("Buzz");
+}
+else {
+console.log (x);
+}
--- statementsAndExpressions/ifElseUsingLogicalOperators.js
+++
@@ -1,9 +0,0 @@
-let age = 15;
-    ssc_score = 970;
-    inter_score = 910;
-    degree_score = 1650;
-if (age <= 14 || ssc_score <=900  ||  inter_score<= 850 || degree_score <= 1500) {
-console.log ("You are not eligible for the course");
-} else {
-  console.log ("You are eligible for the course");
-}
