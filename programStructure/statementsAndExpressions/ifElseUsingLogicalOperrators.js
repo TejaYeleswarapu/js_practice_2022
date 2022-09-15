dab3aa0d3011d4e2bdbe09fd0ce5e01e84e9a31a
TejaYeleswarapu <teja.yeleswarapu@gmail.com>
2022-09-15 21:07:11 +0530

    ifElseUsingLogicalOperrators

---
+++ programStructure/statementsAndExpressions/ifElseUsingLogicalOperrators.js
@@ -0,0 +1,18 @@
+var english_marks = 40;
+var social_marks = 30;
+var geo_marks = 58;
+var history_marks = 80;
+if (english_marks<=35) {
+ console.log("fail");
+}
+  else if (social_marks<=35) { 
+     console.log("fail");
+  }
+else if (geo_marks<=35) { 
+     console.log("fail");
+}
+else if (history_marks<=35) { 
+     console.log("fail");
+} else {
+  console.log("pass");
+}
