228c2c4385f69ac4fcee8779131e65ec4a5addc3
TejaYeleswarapu <teja.yeleswarapu@gmail.com>
2022-09-19 17:48:43 +0530

    tables

---
+++ programStructure/exercises/tables.js
@@ -0,0 +1,6 @@
+for (var multiplication = 2; multiplication <= 10; multiplication++){
+    for (var i = 1; i <= 10; i++) {
+      var result = multiplication * i;
+      console.log(multiplication + '*' + i + ' = ' + result);
+    }
+   }
